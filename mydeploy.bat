cb5da180bfb25e51ffac27683fb223016cb08f92
chenghao <chenghao@bj-fanuc.com.cn>
2018-07-31 22:01:34 +0800

    添加感谢贫穷

---
+++ mydeploy.bat
@@ -0,0 +1,9 @@
+@echo off
+echo "使用bat脚本来复制文件夹和文件"
+xcopy public\*.* ..\masterMyblog\harrycheng.github.io\  /s /e /c /y /h /r
+cd ..\masterMyblog\harrycheng.github.io
+git add .
+git commit -m "auto deploy..."
+git push
+cd ../../hexoblog
+pause
