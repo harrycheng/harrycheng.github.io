2bb51ae754e33e549ad0ec16b97626049c844914
chenghao <chenghao@bj-fanuc.com.cn>
2018-08-01 12:35:29 +0800

    add mydeploy.bat

--- mydeploy.bat
+++ mydeploy.bat
@@ -4,7 +4,7 @@ xcopy public\*.* ..\masterMyblog\harrycheng.github.io\  /s /e /c /y /h /r
 cd ..\masterMyblog\harrycheng.github.io
 git add .
 git commit -m "auto deploy..."
-git push
+git push origin master
 cd ../../hexoblog
 pause
 
