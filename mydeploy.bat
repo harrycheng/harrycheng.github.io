ca704ffa59af848a2229677fa4215d9ff796de95
chenghao <chenghao@bj-fanuc.com.cn>
2021-09-01 14:18:44 +0800

    new evn

--- mydeploy.bat
+++ mydeploy.bat
@@ -1,9 +1,8 @@
 @echo off
-echo "使用bat脚本来复制文件夹和文件"
 xcopy public\*.* ..\masterMyblog\harrycheng.github.io\  /s /e /c /y /h /r
 cd ..\masterMyblog\harrycheng.github.io
 git add .
-git commit -m "auto deploy..."
+git commit -m "auto deploy...%date%%time%"
 git push origin master
 cd ../../hexoblog
 pause
